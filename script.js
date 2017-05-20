34d025f7e8d8be981b8eeb837f2239fb8e070b82
caiodib <dib.caio@gmail.com>
2017-05-20 12:48:23 -0300

    atualizacao slide 5

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    var buttonAbertura = document.querySelector('.modal .fade');
+    function toggleModal(){
+        if(modal.classList.contains('active')){
+            modal.classList.remove('active');
+        }
+        else{
+            modal.classList.add('active');
+        }
+    }
+</script>
